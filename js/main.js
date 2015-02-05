ced589fd94dd5e7539dbdbb7d250aaf2ad62d95a
IGI-111 <igi.222@gmail.com>
2015-02-05 20:34:13 +0100

    added base template

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+$(document).ready(function(){
+	$("#player").load("templates/player.html", setupPlayer);
+})
 
+function setupPlayer() {
+	var audio = $("#player audio")[0];
+	$("#player .playButton").on("click", function(){
+		audio.play();
+	});
+	$("#player .pauseButton").on("click", function(){
+		audio.pause();
+	});
+	$("#player audio").bind("timeupdate", function(){
+		var pos = (audio.currentTime / audio.duration) * 100;
+		$("#player .progress .progress-bar").attr("aria-valuenow", pos);
+		$("#player .progress .progress-bar").width(pos + "%");
+		$("#player .progress .progress-bar span").html(pos + "% Complete");
+	});
+}
