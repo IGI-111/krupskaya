93f0af796f642f7df8ea5f322438d22a51852e5a
IGI-111 <igi.222@gmail.com>
2015-02-09 22:14:33 +0100

    moving to a cleaner model

---
+++ README.md
@@ -0,0 +1,3 @@
+#Krupskaya
+
+This is a simple single page music player written mostly in javascript and hosted at [krupskaya.machinemen.fr](http://krupskaya.machinemen.fr).
--- index.php
+++ index.php
@@ -74,9 +74,6 @@
 					</div>
 				</div>
 			</div>
-			<hr>
-			<footer>
-			</footer>
 		</div> <!-- /container -->        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 		<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 		<script>window.jQuery.ui || document.write('<script src="js/vendor/jquery-ui.min.js"><\/script>')</script>
