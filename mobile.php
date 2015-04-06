692546c4c2e07e02388a95dcd8d75cc8b662c6e0
IGI-111 <igi.222@gmail.com>
2015-04-06 18:09:37 +0200

    still fixing mobile bug

--- mobile.php
+++ mobile.php
@@ -24,8 +24,7 @@
             <nav data-position="fixed" data-role="header" role="navigation">
                 <h1 id="playing">Krupskaya</h1>
                 <div id="player" data-role="control-group" class="ui-btn-left" data-type="horizontal">
-                    <button class="glyphicon glyphicon-play playPauseButton ui-btn"></button>
-                    <audio hidden>
+                    <audio controls style="display: inline; height:30px;" data-role="none">
                         <source src="" id="mp3Source" type="audio/mp3">
                         <source src="" id="oggSource" type="audio/ogg">
                     </audio>
